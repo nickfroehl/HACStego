f434f43f99c35d722745e105af63510ba4022382
Nicholas Froehlinger <nickfroehl@gmail.com>
2021-12-06 18:06:28 -0500

    cheese pizza: see images folder

--- shim.py
+++ shim.py
@@ -9,4 +9,4 @@ def decode(in_img_path, algorithm, arg, out_file_loc_path):
     if (arg == None):
         arg = "0"
     print("./build/hacstego.x "+algorithm+" "+in_img_path+" "+arg+" "+out_file_loc_path)
-    os.system("./build/hacstego.x "+algorithm+" "+in_img_path+" "+arg+" "+out_file_loc_path)
+	os.system("./build/hacstego.x "+algorithm+" "+in_img_path+" "+arg+" "+out_file_loc_path)
